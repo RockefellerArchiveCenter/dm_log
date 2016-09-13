3373ee19c4bd1f34debdd7b79a21cec3a3b82adb
bonniegee <bgordon@rockarch.org>
2016-09-13 12:38:12 -0400

    add error messages

--- app/models/dm_item.rb
+++ app/models/dm_item.rb
@@ -1,3 +1,12 @@
 class DmItem < ApplicationRecord
+  
+  validates_presence_of :auto_id
+  
+  validates_presence_of :refid
+  
+  validates_presence_of :format
+  
+  validates_presence_of :status
+  
   scope :sorted, lambda { order("dm_items.created_at ASC") }
 end
