ac3dce88d171b03170d80fb1769629b96c4bf393
bonniegee <bgordon@rockarch.org>
2016-10-12 11:33:23 -0400

    clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'dm_items#index'
   
-  match ':controller(/:action(/:id))', :via => [:get, :post]
+  # match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
