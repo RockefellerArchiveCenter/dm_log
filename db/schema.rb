9f869e3a91ed2dddb8d257b17707cf7f240bcae9
Hillel Arnold <helrond@hotmail.com>
2019-02-13 16:34:44 -0500

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,25 +12,22 @@
 
 ActiveRecord::Schema.define(version: 20170526144906) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "dm_items", force: :cascade do |t|
+  create_table "dm_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "auto_id"
     t.string   "format"
     t.string   "status"
     t.string   "method"
     t.string   "disposition"
     t.date     "transfer_date"
-    t.text     "notes"
+    t.text     "notes",         limit: 65535
     t.string   "refid"
     t.string   "display_title"
     t.string   "resource"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -39,8 +36,8 @@ ActiveRecord::Schema.define(version: 20170526144906) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string     "current_sign_in_ip"
-    t.string     "last_sign_in_ip"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
