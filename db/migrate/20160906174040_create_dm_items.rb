9654cfe42c2c83f92ab8a2705f3fb86adc139ac9
p-galligan <pjg250@gmail.com>
2019-03-26 13:17:55 -0400

    Revert migration changes
    reverts this commit to remove erroneous migration method

--- db/migrate/20160906174040_create_dm_items.rb
+++ db/migrate/20160906174040_create_dm_items.rb
@@ -12,6 +12,10 @@ class CreateDmItems < ActiveRecord::Migration[5.0]
       t.string :refid
       t.string :display_title
       t.string :resource
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 41b8762... Fixes #44
       t.timestamps
     end
   end
