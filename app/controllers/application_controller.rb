45d65b4e89732ce390a610f5095f1c070649c846
bonniegee <bgordon@rockarch.org>
2019-05-01 16:58:01 -0400

    allow controller to handle token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  
+  acts_as_token_authentication_handler_for User
+  
   protect_from_forgery with: :exception
   
   before_action :authenticate_user!
