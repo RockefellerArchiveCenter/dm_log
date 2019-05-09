fb7fd22689f05fe1dc5701d6c45c0464e81d520c
bonniegee <bgordon@rockarch.org>
2019-05-09 10:38:56 -0400

    fix #55

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: {
+          registrations: 'users/registrations'
+        }
+        
   get 'sessions/new'
 
   get 'signup'  => 'users#new'
