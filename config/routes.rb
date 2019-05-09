40b37bc0f655cc72ef55e1b85572d0f473bd44b1
bonniegee <bgordon@rockarch.org>
2019-05-09 11:46:30 -0400

    add bootstrap styling to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-          registrations: 'users/registrations'
+          registrations: 'users/registrations',
+          sessions: 'users/sessions'
         }
         
   get 'sessions/new'
